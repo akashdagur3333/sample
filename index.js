71cb3df54ae9ace088fc4075cbe5022eb02db9aa
akashdagur3333 <62603200+akashdagur3333@users.noreply.github.com>
2023-04-14 08:03:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http =require('http');
+
+const server=http.createServer((req,res)=>{
+    res.writeHead(200);
+    res.end("hello world")
+});
+const PORT =process.env.PORT || 3000;
+server.listen(PORT,()=>console.log("server is running on port 3000"))
